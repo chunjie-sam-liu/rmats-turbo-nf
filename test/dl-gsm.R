6d8995553d3c61c73b0e568c1e6125b9a617e536
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2021-12-29 15:33:40 -0500

    choose download strategy

--- test/dl-gsm.R
+++ test/dl-gsm.R
@@ -15,52 +15,64 @@ library(GEOquery)
 
 GSE112037 <- GEOquery::getGEO(GEO = "GSE112037", destdir = "test/data", GSEMatrix = TRUE)
 
-GSE112037[[1]] %>%
-  phenoData() %>%
-  pData() %>%
-  tibble::as_tibble() %>%
-  dplyr::mutate(
-    srr = purrr::map(
-      .x = relation.1,
-      .f = function(.x) {
-        .srx <- gsub(pattern = "SRA: ", replacement = "", x = .x)
-        .srx_html <- xml2::read_html(x = .srx)
-        .srx_html %>%
-          rvest::html_nodes('td a') %>%
-          rvest::html_text()
-      }
-    )
-  ) %>%
-  tidyr::unnest() ->
-  GSE112037_1_data
+GSE112037 %>%
+  purrr::map(.f = function(.x) {
+    .x %>%
+      phenoData() %>%
+      pData() %>%
+      tibble::as_tibble() %>%
+      dplyr::mutate(
+        srr = purrr::map(
+          .x = relation.1,
+          .f = function(.x) {
+            .srx <- gsub(pattern = "SRA: ", replacement = "", x = .x)
+            .srx_html <- xml2::read_html(x = .srx)
+            .srx_html %>%
+              rvest::html_nodes('td a') %>%
+              rvest::html_text()
+          }
+        )
+      ) %>%
+      tidyr::unnest()
+  }) %>%
+  dplyr::bind_rows() ->
+  GSE112037_meta
 
-GSE112037[[2]] %>%
-  phenoData() %>%
-  pData() %>%
-  tibble::as_tibble() %>%
-  dplyr::mutate(
-    srr = purrr::map(
-      .x = relation.1,
-      .f = function(.x) {
-        .srx <- gsub(pattern = "SRA: ", replacement = "", x = .x)
-        .srx_html <- xml2::read_html(x = .srx)
-        .srx_html %>%
-          rvest::html_nodes('td a') %>%
-          rvest::html_text()
-      }
-    )
-  ) %>%
-  tidyr::unnest() ->
-  GSE112037_2_data
 
-dplyr::bind_rows(GSE112037_1_data, GSE112037_2_data) %>%
+GSE112037_meta %>%
   readr::write_tsv(file = "test/GSE112037_metadata.tsv")
 
+# Aspera ------------------------------------------------------------------
+
+
+GSE112037_meta %>%
+  dplyr::select(srr) %>%
+  dplyr::mutate(dir = stringr::str_sub(srr, start = 1, end = 6)) %>%
+  dplyr::mutate(path = glue::glue("/sra/sra-instant/reads/ByRun/sra/SRR/{dir}/{srr}")) %>%
+  dplyr::mutate(last = stringr::str_sub(srr, start = -1, end = -1)) %>%
+  dplyr::mutate(ebi = glue::glue("/vol1/fastq/{dir}/00{last}/{srr}")) ->
+  GSE112037_meta_sra_path
+
+GSE112037_meta_sra_path %>%
+  dplyr::select(path) %>%
+  readr::write_tsv(file = "~/scratch/aspera/GSE112037_sra_aspera.txt", col_names = FALSE)
+
+# ascp -v -QT -l 400m -m 300m -P33001 -k1 -i ${HOME}/.aspera/connect/etc/asperaweb_id_dsa.openssh --mode recv --host fasp.sra.ebi.ac.uk --user era-fasp --file-list GSE112037_sra_aspera.txt ./
+
+GSE112037_meta_sra_path %>%
+  dplyr::select(ebi) %>%
+  readr::write_tsv(file = "~/scratch/aspera/GSE112037_sra_ebi_aspera.txt", col_names = FALSE)
+
+#ascp -v -QT -l 2g -m 500m -P33001 -k1 -i ${HOME}/.aspera/connect/etc/asperaweb_id_dsa.openssh --mode recv --host fasp.sra.ebi.ac.uk --user era-fasp --file-list GSE112037_sra_ebi_aspera.txt ./ebi
+
+# No longer use aspera to donwload data from sra, use prefetch.
+# prefetch sra in parallel
+
+GSE112037_meta_sra_path %>%
+  dplyr::select(srr) %>%
+  readr::write_tsv(file = "~/scratch/aspera/GSE112037_sra.txt")
+# prefetch --option-file GSE112037_sra.txt --output-directory ./
 
 # Save image --------------------------------------------------------------
 
-library(SRAdb)
-sra_dbname <- file.path(system.file('extdata', package = "SRAdb"), 'SRAmetadb_demo.sqlite')
-sra_con <- dbConnect(dbDriver("SQLite"), sra_dbname)
-in_acc <- GSE112037_2_data$srr[[1]]
-sraFiles <- ascpSRA( in_acc, sra_con, "ascp -QT -l 300m", fileType = 'sra', destDir=getwd() )
+
