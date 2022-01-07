a72d06f62381c1649f2b6f0af260273247734fa7
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2022-01-07 14:27:00 -0500

    test dump

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: 2022-01-07 14:05:27
+# @DESCRIPTION:
+
+# Number of input parameters
+
+
+nextflow run main.nf \
+  -config test.config \
+  -profile base,singularity \
+  -resume
