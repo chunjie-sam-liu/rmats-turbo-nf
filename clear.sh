cb879e864ab15606bc4f9ca099740ae5b6c77803
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2022-01-07 14:26:55 -0500

    clear

---
+++ clear.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# @AUTHOR: Chun-Jie Liu
+# @CONTACT: chunjie.sam.liu.at.gmail.com
+# @DATE: 2022-01-05 15:03:37
+# @DESCRIPTION:
+
+# Number of input parameters
+cd ${HOME}/github/rmats-turbo-nf/
+rm -rf .nextflow*
+rm -rf work/
+rm -rf results/
+rm -rf report*
+rm -rf timeline*
+rm -rf trace*
+rm -rf dag*
+rm -rf outdir
+rm -rf my-results
+rm -rf pipeline_*
+rm -rf ${HOME}/tmp/work
